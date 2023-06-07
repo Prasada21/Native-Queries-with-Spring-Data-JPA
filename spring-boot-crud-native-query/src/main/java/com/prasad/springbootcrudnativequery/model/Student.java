e792ed72efca93c92f68d7ea21c43fe5537e5bab
Prasad <prasada@ttechlab.com>
2023-06-07 12:15:21 +0530

    "removed @data and added getter and setter"

--- spring-boot-crud-native-query/src/main/java/com/prasad/springbootcrudnativequery/model/Student.java
+++ spring-boot-crud-native-query/src/main/java/com/prasad/springbootcrudnativequery/model/Student.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 
 import lombok.Data;
 
-@Data
+
 @Entity
 public class Student {
 
@@ -20,6 +20,32 @@ public class Student {
 	private String name;
 	private String course;
 	private int age;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
 	
 	
 }
